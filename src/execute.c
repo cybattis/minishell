cdd14433d32ce3a2c6f45fdf282925a2174cde3d
Cyril Battistolo <cybattis@student.42lyon.fr>
2022-02-20 11:59:33 +0100

    FIX: execture_extern()

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: cybattis <cybattis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 17:10:07 by cybattis          #+#    #+#             */
-/*   Updated: 2022/02/19 17:57:34 by cybattis         ###   ########.fr       */
+/*   Updated: 2022/02/20 11:40:03 by cybattis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,20 @@ int	execute_cmd(t_command_batch cmd_batch)
 
 static int	execute_builtin(t_command_batch cmd_batch, size_t i)
 {
-	if (!ft_strcmp(cmd_batch.commands[i].name, "exit"))
-		bt_exit();
-	else if (!ft_strcmp(cmd_batch.commands[i].name, "cd"))
+	if (!ft_strcmp(cmd_batch.commands[i].name, "cd"))
 		bt_cd(cmd_batch.commands[i].args[1]);
 	else if (!ft_strcmp(cmd_batch.commands[i].name, "echo"))
 		bt_echo(cmd_batch.commands[i]);
 	else if (!ft_strcmp(cmd_batch.commands[i].name, "pwd"))
 		bt_pwd();
+	else if (!ft_strcmp(cmd_batch.commands[i].name, "unset"))
+		;
+	else if (!ft_strcmp(cmd_batch.commands[i].name, "env"))
+		;
+	else if (!ft_strcmp(cmd_batch.commands[i].name, "export"))
+		;
+	else if (!ft_strcmp(cmd_batch.commands[i].name, "exit"))
+		bt_exit();
 	return (0);
 }
 
@@ -54,8 +60,7 @@ static int	execute_extern(t_command_batch cmd_batch, size_t i)
 	pid = fork();
 	if (!pid)
 	{
-		printf("cmd:%s args:%s\n", cmd_batch.commands[i].name, cmd_batch.commands[i].args[i]);
-		if (!execve(cmd_batch.commands[i].name, ft_strjoin("./cat", cmd_batch.commands[i].args, 0), NULL))
+		if (!execve(cmd_batch.commands[i].name, cmd_batch.commands[i].args, NULL))
 			ft_errno_exit(errno);
 	}
 	else if (pid > 0)
