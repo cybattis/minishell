9e0fa2cfd1e10c3a767b3005192a3b188e5cc8ee
Nathan <nathanjsim42@gmail.com>
2022-02-21 20:22:36 +0100

    Modified prompt generation: supported detection of git and git branches

--- src/io.c
+++ src/io.c
@@ -12,7 +12,6 @@
 
 #include "libft.h"
 #include "minishell.h"
-#include <sys/param.h>
 
 char	*ft_get_line(void)
 {
@@ -26,19 +25,4 @@ char	*ft_get_line(void)
 	if (line_read && *line_read)
 		add_history(line_read);
 	return (line_read);
-}
-
-char	*get_prompt(void)
-{
-	static char	*prompt;
-	char		buf[MAXPATHLEN];
-
-	if (prompt)
-		free(prompt);
-	getcwd(buf, MAXPATHLEN);
-	prompt = ft_strjoin("\e[1;93mMinishell\e[0m: \e[1;94m", buf, 0);
-	prompt = ft_strjoin(prompt, "\e[0m", 1);
-	prompt = ft_strappend(prompt, '$');
-	prompt = ft_strappend(prompt, ' ');
-	return (prompt);
 }
