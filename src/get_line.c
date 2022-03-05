af6a8c18fcd2e78ef6ff50d9b49ba3c790927996
Cyril Battistolo <cybattis@student.42lyon.fr>
2022-03-05 16:38:59 +0100

    FIX: typo files WIP: add write to prompt func -- trying to print exit on the same line      as the prompt when exiting

--- src/get_line.c
+++ src/get_line.c
@@ -1,18 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   io.c                                               :+:      :+:    :+:   */
+/*   get_line.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cybattis <cybattis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 12:36:16 by cybattis          #+#    #+#             */
-/*   Updated: 2022/02/23 12:02:54 by cybattis         ###   ########.fr       */
+/*   Updated: 2022/03/05 16:36:53 by cybattis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
 #include "readline.h"
 #include "minishell.h"
+
+static int	write_to_prompt(char *msg);
 
 char	*ft_get_line(void)
 {
@@ -22,8 +26,23 @@ char	*ft_get_line(void)
 		gc_free(get_gc(), line_read);
 	line_read = readline(get_prompt());
 	if (!line_read)
-		bt_exit();
+		write_to_prompt("exit\n");
 	if (line_read && *line_read)
 		add_history(line_read);
 	return (line_read);
+}
+
+static int	write_to_prompt(char *msg)
+{
+	int		fd;
+	char	*tty_name;
+
+	tty_name = ttyname(STDIN_FILENO);
+	fd = open(tty_name, O_WRONLY);
+	if (fd == -1)
+		return (ft_errno(errno));
+	write(fd, msg, ft_strlen(msg));
+	gc_clean(get_gc());
+	exit(EXIT_SUCCESS);
+	return (0);
 }
